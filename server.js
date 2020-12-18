4bf416f2886f27e8d0ccbd6427dabecfa6e0063b
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-12-17 18:18:11 -0800

    heroku connect test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -9,15 +10,15 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api", allRoutes);
-
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build')); // serve the static react app
-    // app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-    //   res.sendFile(path.join(__dirname, './client/build/index.html'));
-    // });
+    app.get('/api', (req, res) => { // don't serve api routes to react app
+      res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+    });
     // console.log('Serving React App...');
 }
+
+app.use("/api", allRoutes);
 
 db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
